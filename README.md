42232a651343386b14930dd11b6eeea929bc241d
elenfe <elenfe7@gmail.com>
2018-03-06 00:34:11 -0300

    gravando o formulário

--- README.md
+++ README.md
@@ -5,15 +5,11 @@ Tecnologias Empregadas
 HTML5
 CSS3
 AngularJs
+Jquery
 Bootstrap
 
 Para iniciar o projeto, colar o link abaixo em um navegador web.
 https://elenfe.github.io/elenfe-plano/#!/home
-
-
-OBS:
-O projeto não está finalizado ainda. Os dados exibidos na tela estão hardcoded na tela. Precisando ser refatorado.
-Faltando fazer as chamadas nas APIs. Armazenar os dados do usuário digitados no formulário e exibir no console. 
 
 
 Autor:
--- controllers/formulario.controller.js
+++ controllers/formulario.controller.js
@@ -13,12 +13,36 @@
     function FormularioController($state, $http) {
         var vm = this;
 
-        vm.salvarEscolha = function(id) {
-            localStorage.setItem("escolhido", id);
-            // console.log(localStorage.getItem("escolhido"));
-            localStorage.getItem("escolhido");
-        }
-        console.log(localStorage.getItem("escolhido"));
-      
+        vm.salvarformulario = function() {            
+                var nome = localStorage.getItem("nome");
+                var email = localStorage.getItem("email");
+                var nascimento = localStorage.getItem("nascimento");
+                var cpf = localStorage.getItem("cpf");
+                var telefone = localStorage.getItem("telefone");
+                
+                if(nome !== null){
+                   $("#nome").val(nome);
+                }
+                if(email !== null){
+                  $("#email").val(email);
+                }
+                if(nascimento !== null){
+                    $("#data").val(nascimento);
+                 }
+                 if(cpf !== null){
+                    $("#cpf").val(cpf);
+                 }
+                 if(telefone !== null){
+                    $("#tel").val(telefone);
+                 }
+                            
+                localStorage.setItem("nome",  $("#nome").val());
+                localStorage.setItem("email",  $("#email").val());
+                localStorage.setItem("nascimento",  $("#data").val());
+                localStorage.setItem("cpf",  $("#cpf").val());
+                localStorage.setItem("telefone",  $("#tel").val());
+                $("#enviado").html("Registro enviado!");
+              
+        }      
     }
 })();
